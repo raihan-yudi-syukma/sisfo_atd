d7e1216b88916f6a6c07b9b04cd8b55af9dc501d
Raihan Yudi Syukma <153145965+raihan-yudi-syukma@users.noreply.github.com>
2023-12-23 23:52:35 +0700

    Update navbar.php

--- application/views/templates/navbar.php
+++ application/views/templates/navbar.php
@@ -1,54 +1,53 @@
-<nav class="main-header navbar navbar-expand" style="background-color: crimson">
+<nav class="main-header navbar navbar-expand" style="background: crimson">
 
-  <ul class="navbar-nav" >
+  <ul class="navbar-nav">
      <!-- pushmenu -->
      <li>
-        <a class="nav-link text-white" title="Hide or show the sidebar" data-widget="pushmenu" href="#" role="button">
+        <a class="nav-link text-white" role="button" title="Hide or show the sidebar" data-widget="pushmenu" href="#">
           <i class="fas fa-bars"></i>
         </a>
       </li>
 
     <!-- home -->
     <li>
-      <a title="Go to homepage" href="<?= site_url() ?>" class="nav-link text-white">
-        <i class="fas fa-home"></i>
-        <b>Home</b>
+      <a class="nav-link text-white font-weight-bold" title="Go to homepage" href="<?= site_url() ?>">
+        <i class="fas fa-home"></i> Home
       </a>
     </li>
 
     <!-- article -->
     <li>
-      <a title="See our published articles" href="<?= site_url('article') ?>" class="nav-link text-white">
-        <i class="fas fa-book"></i>
-        <b>Artikel</b>
+      <a class="nav-link text-white font-weight-bold" title="See our published articles" href="<?= site_url('article') ?>">
+        <i class="fas fa-book"></i> Artikel
       </a>
     </li>
 
       <!-- about -->
     <li>
-      <a title="About ATD" href="<?= site_url('page/about') ?>" class="nav-link text-white">
-        <i class="fas fa-info"></i>
-        <b>About Us</b>
+      <a class="nav-link text-white font-weight-bold" title="About ATD" href="<?= site_url('page/about') ?>">
+        <i class="fas fa-info"></i> About Us
       </a>
     </li>
 
     <!-- contact -->
     <li>
-      <a title="Send a message to us!" href="<?= site_url('page/contact') ?>" class="nav-link text-white">
-        <i class="fas fa-comment text-teal"></i>
-        <b>Contact</b>
+      <a class="nav-link text-white font-weight-bold" title="Send a message to us!" href="<?= site_url('page/contact') ?>">
+        <i class="fas fa-comment text-teal"></i> Contact
       </a>
     </li>
+  </ul>
+  <!-- /. Left navbar links -->
 
+  <!-- Right navbar links -->
+  <ul class="navbar-nav ml-auto">
     <!-- login-->
     <?php if ($current_user == FALSE) : ?>
-      <li style="margin-left: auto">
-        <a title="Login if you're an administrator" href="<?= site_url('auth/login') ?>" class="nav-link text-white">
-          <i class="fas fa-key text-lime"></i>
-          <b>Login</b>
+      <li class="pr-5">
+        <a class="nav-link btn bg-lime font-weight-bold" role="button" title="Login if you're an administrator" href="<?= site_url('auth/login') ?>">
+          <i class="fas fa-key"></i> Login
         </a>
       </li>
     <?php endif; ?>
   </ul>
-  <!-- /. Left navbar links -->
+  <!-- /. Right navbar links -->
 </nav>
